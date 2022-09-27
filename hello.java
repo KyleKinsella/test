64db39adbb26217916867b6ab4dfa5756a87c13e
KyleKinsella <C00273146@itcarlow.ie>
2022-09-27 14:51:35 +0100

    added another line of code

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hello {
     public static void main(String[] args) {
         System.out.print("Hello Github! My name is Kyle K");
+        System.out.print("I got my code up to Github!");
     }
 }
